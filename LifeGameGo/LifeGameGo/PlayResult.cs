baf459d222c0a894b0dc084f60eb438d06e6a666
nagata442000 <nagata442000@yahoo.co.jp>
2015-07-26 19:46:47 +0900

    random ai implement.

---
+++ LifeGameGo/LifeGameGo/PlayResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGameGo
+{
+    class PlayResult
+    {
+        public PointSet valid;
+        public PointSet invalid;
+
+        public PlayResult(Board board)
+        {
+            valid = new PointSet();
+            invalid = new PointSet();
+            Point p = new Point();
+
+            for(int x=0;x<Info.N;++x)
+            {
+                for (int y = 0; y < Info.N; ++y)
+                {
+                    p.x = x;
+                    p.y = y;
+                    Info.GameState s = board.test_play(p);
+                    switch(s)
+                    {
+                        case Info.GameState.VALID_MOVE:
+                            valid.set(p);
+                            break;
+                        case Info.GameState.INVALID_MOVE:
+                            invalid.set(p);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LifeGameGo/LifeGameGo/RandomAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGameGo
+{
+    public class RandomAI : AI
+    {
+        private Random rnd;
+        public RandomAI()
+        {
+            rnd = new Random();
+        }
+
+        public Point play(Board board)
+        {
+            PlayResult r = new PlayResult(board);
+            List<Point> l = r.valid.toList();
+            if(l.Count ==0)
+            {
+                return Point.Pass;
+            }
+            return l[rnd.Next(l.Count)];
+        }
+
+    }
+}
